d089cefb74a9c04861d13c43868b239b5dc6591f
DanielRomo15 <137075635+DanielRomo15@users.noreply.github.com>
2025-11-07 09:13:40 -0500

    Update views.py
    listar adopcion

--- adopcion/Aplicaciones/mascota/views.py
+++ adopcion/Aplicaciones/mascota/views.py
@@ -120,9 +120,9 @@ def eliminar_mascota(request, id):
 
 
 
-def listar_adopciones(request)
+def listar_adopciones(request):
     adopciones = Adopcion.objects.select_related('persona', 'mascota').all()
-    return render(request, 'mascotas/listar_adopciones.html', {'adopciones': adopciones})
+   
 
 def crear_adopcion(request):
     personas = Persona.objects.all()
